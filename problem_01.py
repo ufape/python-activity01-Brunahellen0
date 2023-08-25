5e88b5bcf98e9a203105768e83f1f588e521e879
Brunahellen0 <142901267+Brunahellen0@users.noreply.github.com>
2023-08-25 13:00:40 -0300

    Update problem_01.py

--- problem_01.py
+++ problem_01.py
@@ -25,3 +25,30 @@ def main():
 
 if __name__ == '__main__':
     main()
+# -*- coding: utf-8 -*-
+
+# YOUR FULL NAME
+# UAG00098
+# Problem Set 1 - Problem 1
+# Description:
+
+"""
+Inputs, Processes and Output (IPO)
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+Input(s):
+Este problema não possui nenhuma entrada.
+
+Processes:
+Você deve imprimir a mensagem "Hello, World!"
+
+Output(s):
+Hello, World!
+"""
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == '__main__':
+    main()
